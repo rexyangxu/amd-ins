b007801284bf938a5f2bd1ebce49c75b7e489500
rexyangxu <88847427+rexyangxu@users.noreply.github.com>
2021-08-13 01:04:51 +0800

    Update and rename amd_ins.sh to amd_install.sh

--- amd_install.sh
+++ amd_install.sh
@@ -80,17 +80,17 @@ case $opt in
     ;;
     "19.10-785425-ubuntu-18.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-19.10-785425-ubuntu-18.04.tar.xz
-        version="19.10-785425-ubuntu-18.04";break
-    ;;
-	"19.10-725072-ubuntu-18.04")
-	wget http://download.hiveos.farm/drivers/amdgpu-pro-19.10-785425-ubuntu-18.04.tar.xz
-        version="19.10-725072-ubuntu-18.04";break
+	version="19.10-785425-ubuntu-18.04";break
+	;;
+    "19.10-725072-ubuntu-18.04")
+    	wget http://download.hiveos.farm/drivers/amdgpu-pro-19.10-785425-ubuntu-18.04.tar.xz
+	version="19.10-725072-ubuntu-18.04";break
 	;;
 	"19.20-812932-ubuntu-18.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-19.20-812932-ubuntu-18.04.tar.xz
-        version="19.20-812932-ubuntu-18.04";break	
-    ;;
-	"19.30-838629-ubuntu-18.04")
+	version="19.20-812932-ubuntu-18.04";break	
+	;;
+    "19.30-838629-ubuntu-18.04")
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-19.30-838629-ubuntu-18.04.tar.xz
         version="19.30-838629-ubuntu-18.04";break
     ;;
@@ -98,9 +98,9 @@ case $opt in
 	wget http://download.hiveos.farm/drivers/amdgpu-pro-19.50-967956-ubuntu-18.04.tar.xz
         version="19.50-967956-ubuntu-18.04";break
 	;;
-  	"21.30-1286092-ubuntu-18.04")
+	"21.30-1286092-ubuntu-18.04")
 	wget https://fichiers.touslesdrivers.com/69444/amdgpu-pro-21.30-1286092-ubuntu-18.04.tar.xz
-        version="21.30-1286092-ubuntu-18.04";break
+	version="21.30-1286092-ubuntu-18.04";break
 	;;
 	"Quit")
 	exit
